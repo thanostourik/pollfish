b786d759259f502e82889485baedf786ce888108
Thanos Tourikas <thanos.tourik@gmail.com>
2020-02-18 23:38:42 +0200

    properly fix initialize database bash script

--- release/initialize-database.sh
+++ release/initialize-database.sh
@@ -1,9 +1,10 @@
-CREATE KEYSPACE pollfish_logging WITH REPLICATION = { 'class': 'SimpleStrategy', 'replication_factor': 1 };
-USER pollfish_logging;
-CREATE TABLE Event (
-  id text PRIMARY KEY,
-  v smallint,
-  time timestamp,
-  m text,
-  comment text
-);
+cqlsh -e "DROP KEYSPACE IF EXISTS pollfish_logging;"
+cqlsh -e "CREATE KEYSPACE pollfish_logging WITH REPLICATION = { 'class': 'SimpleStrategy', 'replication_factor': 1 };"
+cqlsh -e "USE pollfish_logging;\
+CREATE TABLE Event (\
+  id text PRIMARY KEY,\
+  v smallint,\
+  time timestamp,\
+  m text,\
+  comment text\
+);"
